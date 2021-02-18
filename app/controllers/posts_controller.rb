ee454fe9c4329a8bfef16d55f08fa703096b53b6
saimaAnis21 <saima_rehman21@yahoo.com>
2021-02-18 20:51:44 +0500

    Add the new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,27 @@
 class PostsController < ApplicationController
+  # before_action :authenticate_user!, only: %i[ new create ]
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      
+      redirect_to new_post_path
+    else
+      
+      render :new
+    end
   end
 
   def index
   end
+
+  private
+
+  def post_params
+    params.permit(:title, :body)
+  end
+
 end
