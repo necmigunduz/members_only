78147030a765a921cf0a19754957dce06ef4faed
necmigunduz <necmigunduz@gmail.com>
2021-02-19 13:36:29 +0300

    Add functionality to the post controller and displaying all the posts on posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,26 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!, only: %i[ new create ]
+  before_action :authenticate_user!, only: %i[ new create ]
+  
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
-    if @post.save
-
-      redirect_to new_post_path
-    else
-      
-      render :new
+    @post = current_user.posts.build(post_params)
+    
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   private
