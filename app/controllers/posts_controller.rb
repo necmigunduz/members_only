81186edae7d0debe22924878c34ad8cbf1a21f5b
saimaAnis21 <saima_rehman21@yahoo.com>
2021-02-19 18:35:43 +0500

    Add functionality for posting with userid

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[ new create ]
   
   def new
-    @post = current_user.posts.build
+    @post = Post.new
   end
 
   def create
@@ -21,6 +21,8 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order('created_at DESC')
+    # @post = Post.new 
+    @post = current_user.posts.build
   end
 
   private
